920a792fd42fef30f4bcc51d36ec3b0ab305cc81
antolikjan <antolikjan@gmail.com>
2023-04-19 16:26:23 +0200

    forgot to add the ui_framework directory

---
+++ scripts/ui_framework/analysis_panel.py
@@ -0,0 +1,107 @@
+from bokeh.models import Row, Div, Spacer, Panel
+from scripts.data import categories, filter_data, cross_corr, both_valid, data_aquisition_overlap
+from functools import partial
+
+
+class AnalysisPanel():
+      """
+      Panel is a high level UI concept based around following assumptions:
+
+      * There are three main elements - Widgets, Data and Plots
+      * There is a bar of Widget elements on the left that are all created inserted in the *ui_elements* dictionary in the constructor 
+        and composed into a layout by the *compose_widgets* function.
+      * All the plots are on the right. They are also created and interpreted in the *plots* dictionary in the constructor
+        and composed into a layout by the *compose_plots* function.
+      * There is data that are plotted - these are in the *data_sources* dictionary. Data are plotted in the plots. 
+      * Changes to the ui_elements cause changes to other ui elements (the *update_widgets* function).
+      * Changes to the ui_elements cause changes to data (the *update_data* function).
+      * Whenever data is changed all the plots are updated (the *update_plots* function).
+      * The most reusable and transparent design has minimal content in the *update_widgets* and *update_plots* and most functionality in *update_data*
+      """
+
+      def __init__(self,data,categories,metadata,title):
+          self.raw_data = data
+          self.categories = categories
+          self.metadata = metadata
+          self.data_sources = {}
+          self.ui_elements = {}
+          self.plots = {}
+          self.title = title
+
+      def update_widgets(self):
+          """
+          This function updates other widgets in response to change in a widget.
+          """
+          pass
+
+
+      def update_data(self):
+          """
+          This function updates all the data in *data_sources*. It is triggered after any change to ui. It is expected to be overridden.
+          """
+          pass
+
+      def update_plots(self):
+          """
+          This function updates all the plots. It is triggered after any change to data. It is expected to be overridden.
+          """
+          pass
+
+      def compose_widgets(self):
+          """
+          This function is expected to compose the bar of widgets. 
+
+          It should return a layout.
+          """
+          pass
+
+      def compose_plots(self):
+          """
+          This function is expected to compose all the plots that will sit on the right. 
+
+          It should return a layout.
+          """        
+          pass
+
+      def compose_panel(self):
+          """
+          This function takes the layouts created by *compose_ui* and *compose_plots* and turns them into panel which is returned.
+          """
+          layout = Row(self.compose_widgets(),Spacer(width=20),self.compose_plots(),sizing_mode="stretch_both")
+            
+          panel = Panel(child=layout, title=self.title)
+
+          self.update_widgets()        
+          self.update_data()        
+          self.update_plots()
+
+          return panel
+
+
+      def update(self,attr,old,new):
+          print('Update: ')
+          print(attr)
+          print(old)
+          print(new)
+          print('-----------------')
+          self.update_widgets()        
+          self.update_data()        
+          self.update_plots()
+          print('Finished')
+
+      def register_widget(self,widget,widget_name,actions_to_respond_to):
+          """
+          This is how one registers widget in the UI.
+
+          Parameters:
+          widget (object): the widget to register
+          widget_name (str): the name under which to register it
+          actions_to_respond_to (list): the list of actions which need to trigger update
+          """
+          assert widget_name not in self.ui_elements, "UI element %s has already been registered" % (widget_name)              
+          self.ui_elements[widget_name] = widget
+          for action in actions_to_respond_to:
+              self.ui_elements[widget_name].on_change(action, self.update)
+
+
+
---
+++ scripts/ui_framework/paired_analysis.py
@@ -0,0 +1,83 @@
+from bokeh.models import Select, Column, Row, LinearAxis, Slider, CheckboxGroup, Div, Spacer, Label, Slope, RadioButtonGroup
+from bokeh.models import ColumnDataSource
+from scripts.data import filter_data
+from .analysis_panel import AnalysisPanel
+
+class PairedAnalysis(AnalysisPanel):
+
+      def __init__(self,data,categories,metadata,title):
+          AnalysisPanel.__init__(self,data,categories,metadata,title)
+
+          ### WIDGETS
+          self.register_widget(Select(title="Category",  options=list(categories.keys()), value = 'Fitbit'),'select_category1',['value'])
+          self.register_widget(Select(title = 'Name', value = 'Steps', options = list(categories['Fitbit'])),'select_variable1',['value'])
+
+          self.register_widget(Select(title="Category",  options=['None']+list(categories.keys()), value = 'Fitbit'),'select_category2',['value'])
+          self.register_widget(Select(title = 'Name', value = 'RHR', options = list(categories['Fitbit'])),'select_variable2',['value'])
+
+          self.register_widget(Select(title="Weighted average",  options=['None','Gauss','PastGauss','FutureGauss'], value = 'None'),"select_filter1",['value'])
+          self.register_widget(Select(title="Weighted average",  options=['None','Gauss','PastGauss','FutureGauss'], value = 'None'),"select_filter2",['value'])
+
+          self.register_widget(Slider(start=0.2, end=10, value=0.2, step=0.2, title="Sigma"),"select_sigma1",['value'])
+          self.register_widget(Slider(start=0.2, end=10, value=0.2, step=0.2, title="Sigma"),"select_sigma2",['value'])
+
+          self.register_widget(RadioButtonGroup(labels=["Var2 -> Var1","no shift","Var1 -> Var2"], active=1),'shift_button_group',['active'])
+
+          ### DATA
+          self.data_sources['raw_data'] = ColumnDataSource(data={'x_values' : data.index,'y_values1' : data['Steps'],'y_values2' : data['RHR'],'y_values_filtered' : data['Steps'],'y_values_filtered' : data['RHR'],'filter1' : data['RHR']*0,'filter2' : data['RHR']*0})
+
+      def compose_widgets(self):
+          widgets_var1 = Column(Div(text="""<b>Varibale 1</b>"""),self.ui_elements["select_category1"], self.ui_elements["select_variable1"],self.ui_elements["select_filter1"],self.ui_elements["select_sigma1"],sizing_mode="fixed", width=120,height=500)
+          widgets_var2 = Column(Div(text="""<b>Varibale 2</b>"""),self.ui_elements["select_category2"], self.ui_elements["select_variable2"],self.ui_elements["select_filter2"],self.ui_elements["select_sigma2"],sizing_mode="fixed", width=120,height=500)  
+          w1 = Row(widgets_var1,widgets_var2,width=240)
+          w2 = Column(w1,Div(text="""<hr width=240px>"""),self.ui_elements["shift_button_group"],width=240)
+          return w2
+
+      def update_widgets(self):
+          self.ui_elements["select_variable1"].options = list(self.categories[self.ui_elements["select_category1"].value])
+
+          if self.ui_elements["select_category2"].value != 'None':
+            self.ui_elements["select_variable2"].options = list(self.categories[self.ui_elements["select_category2"].value])
+          else:
+            self.ui_elements["select_variable2"].options = ['None']
+
+          if self.ui_elements["select_variable1"].value not in self.categories[self.ui_elements["select_category1"].value]:
+             self.ui_elements["select_variable1"].value = self.categories[self.ui_elements["select_category1"].value][0]
+
+          if self.ui_elements["select_category2"].value != 'None':
+            if self.ui_elements["select_variable2"].value not in self.categories[self.ui_elements["select_category2"].value]:
+               self.ui_elements["select_variable2"].value = self.categories[self.ui_elements["select_category2"].value][0]
+
+      def update_data(self):
+
+          d1 = self.raw_data[self.ui_elements["select_variable1"].value].copy()
+          if self.ui_elements["select_category2"].value != None:
+             d2 = self.raw_data[self.ui_elements["select_variable2"].value].copy()
+          
+          if self.ui_elements['shift_button_group'].active == 0:
+             d1[0:-1] = d1[1:]
+             d1[-1] = float('NaN')
+
+          if self.ui_elements['shift_button_group'].active == 2:
+             d2[0:-1] = d2[1:]
+             d2[-1] = float('NaN')
+
+          self.data_sources['raw_data'].data['y_values1'] = d1
+          self.data_sources['raw_data'].data['y_values2'] = d2
+
+          if self.ui_elements["select_filter1"].value == 'None':
+             self.data_sources['raw_data'].data['y_values_post_processed1'] = self.data_sources['raw_data'].data['y_values1']
+             self.data_sources['raw_data'].data['filter1'] *=0
+          else:
+             filtr,result1 = filter_data(self.ui_elements["select_filter1"].value,self.data_sources['raw_data'].data['y_values1'],self.ui_elements["select_sigma1"].value)
+             self.data_sources['raw_data'].data['y_values_post_processed1'] = result1
+             self.data_sources['raw_data'].data['filter1'] = filtr/filtr.max()
+
+          if self.ui_elements["select_filter2"].value == 'None':
+             self.data_sources['raw_data'].data['y_values_post_processed2'] = self.data_sources['raw_data'].data['y_values2']
+             self.data_sources['raw_data'].data['filter2'] *=0
+          else:
+             filtr,result2 = filter_data(self.ui_elements["select_filter2"].value,self.data_sources['raw_data'].data['y_values2'],self.ui_elements["select_sigma2"].value)
+             self.data_sources['raw_data'].data['y_values_post_processed2'] = result2
+             self.data_sources['raw_data'].data['filter2'] = filtr/filtr.max()
+
