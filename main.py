a368a23b18a136e0371bc70985b7a7649670699c
Jan Antolik <antolikjan@gmail.com>
2023-07-30 22:29:31 +0200

    minor changes

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ cache=False
 
 df,metadata,categories = load_tables(cf.config['tables_to_load'],api_key=cf.config['airtable_api_key'],base_id=cf.config['airtable_base_id'],cache=cache)
 
-rm = RelationshipMetadata(metadata,initialize=False)
+rm = RelationshipMetadata(metadata,initialize=True)
 
 curdoc().add_root(create_app(df,metadata,categories,rm))
 #blood_tests = load_blood_tests(['WBC Rest','WBC Differential','Immunity','Minerals','Kidney Function','Liver Function','Pancreas','Proteins','Glucose','Lipids','Thyroid','Hormones','Vitamins','Prostate','BiologicalAgeScores','Cardio'],api_key=cf.config['airtable_api_key'],base_id=cf.config['airtable_base_id'],cache=cache)
