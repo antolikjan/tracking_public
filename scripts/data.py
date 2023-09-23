2f1a22c0b3fe6b49f3fc26940ed1bd691d367d74
Tamar Noselidze <takonoselidze@gmail.com>
2023-09-23 19:46:12 +0200

    sorting bloodtests dataframe by the date index

--- scripts/data.py
+++ scripts/data.py
@@ -163,8 +163,9 @@ def load_blood_tests(api_key,base_id,cache=False):
         blood_tests = {}
         for view in views:
             blood_tests[view] = convert_to_dataframe(table.all(fields = views[view]+["Date"]),index_column="Date", datatime_index=True)
-            # blood_tests[view].sort_values(by="Date", inplace=True)  # Sort the DataFrame by Date
+            blood_tests[view].sort_index(inplace=True)
             print(blood_tests[view].columns)
+            
         # cache the data
         pickle.dump(blood_tests,open('./locals/cache_bt.pickle','bw'))
     else:
